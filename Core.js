4d94ce8fdbe9b1dfe658febcac7b91e683ff5d64
Riders004 <120787636+Riders004@users.noreply.github.com>
2023-02-22 13:01:27 +0000

    Update Core.js

--- Core.js
+++ Core.js
@@ -4990,7 +4990,19 @@ break
   
 
 
- case "tts":  case texttospeech":  case "say": case "speak":{
+ case 'add':{     			
+    if (!m.isGroup) return replay(mess.grouponly)
+/* if (!isBotAdmins) return replay(mess.botadmin)
+ let users = m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+ if (users.length == 0) return replay(`Please write the number of the person you want to add to thhis group`)
+  await Miku.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => replay(`User Added Successfully!`)).catch((err) => replay(`Cannot add that user to this group!`))
+ } */
+reply(`TYPE {prefix}help 0r {prefix}menu TO GET LIST`)
+ break
+
+
+
+ case "tts":  case "texttospeech":  case "say": case "speak":{
     if (isBan) return reply(mess.banned)	 			
     if (isBanChat) return reply(mess.bangc)
 
